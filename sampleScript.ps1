3c57f8fc3ae1e30892ae4cb1a3113ac8ce24d893
kongou-ae <y.matsumoto.ae@gmail.com>
2022-06-28 15:15:27 +0900

    fixed a sample script

--- sampleScript.ps1
+++ sampleScript.ps1
@@ -2,6 +2,6 @@
 New-AzSubscriptionDeployment -Name (Get-date -format yyMMdd) -Location "japaneast" -TemplateFile .\main.bicep -TemplateParameterFile "param.json"
 
 # Deleting resources
-Remove-AzResourceGroup -Name rg-net-opslab -Force 
-Remove-AzResourceGroup -Name rg-mgmt-opslab -Force -AsJob
-Remove-AzResourceGroup -Name rg-vm-opslab -Force -AsJob
+Remove-AzResourceGroup -Name rg-net-opslab-eval -Force -AsJob
+Remove-AzResourceGroup -Name rg-mgmt-opslab-eval -Force -AsJob
+Remove-AzResourceGroup -Name rg-vm-opslab-eval -Force -AsJob
